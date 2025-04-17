1547652b35151a76454418563e9739c99616b161
Mayra Landa <mlandae16@gmail.com>
2025-04-17 14:16:46 -0700

    debugging

--- index.js
+++ index.js
@@ -85,6 +85,10 @@ app.use(
     },
   })
 );
+app.use((req, res, next) => {
+  console.log("Session object:", req.session);
+  next();
+});
 
 app.use(passport.initialize());
 app.use(passport.session());
